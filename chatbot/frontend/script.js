01e89d7aed745fe380684fde31c9464cb427c536
Suyalvivek <suyal10vivek@gmail.com>
2025-12-09 11:54:44 +0530

    integrating server with ui

--- chatbot/frontend/script.js
+++ chatbot/frontend/script.js
@@ -4,25 +4,50 @@ const askBtn = document.querySelector('#ask')
 console.log(input);
 input.addEventListener('keyup',handleEnter);
 askBtn.addEventListener('click',handleAsk);
-function generate(text){
+async function generate(text){
+    //append msg to ui
     const msg = document.createElement("div");
     msg.className=`my-6 bg-neutral-600 p-3 rounded-xl ml-auto max-w-fit`
     msg.textContent=text;
     chatContainer?.appendChild(msg);
     input.value=''
-
+    // send it to llm
+    const assistantMessage = await callServer(text);
+    console.log(assistantMessage);
+    //apend assistant msg to ui
+    const assistantMsg = document.createElement("div");
+    assistantMsg.className=`my-6 bg-neutral-800 p-3 rounded-xl mr-auto max-w-fit`
+    assistantMsg.textContent=assistantMessage;
+    chatContainer?.appendChild(assistantMsg);
 }
-function handleAsk(e){
+async function callServer(inputText) {
+    const response = await fetch('http://localhost:3000/chat', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ message: inputText })
+    });
+
+    if (!response.ok) {
+        throw new Error('Error generating response');
+    }
+
+    const result = await response.json();
+    console.log("Server result:", result);
+    return result.message;  // THIS is assistantMessage
+}
+async function handleAsk(e){
          const text = input.value.trim();
         if(!text) return;
-        generate(text);
+        await generate(text);
 
 }
-function handleEnter(e){
+async function handleEnter(e){
     if(e.key==='Enter'){
         const text = input.value.trim();
         if(!text) return;
-        generate(text);
+        await generate(text);
         console.log(text);
     }
 }
--- chatbot/server.js
+++ chatbot/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { generate } from './chatbot.js';
+import cors from 'cors';
 
 const app = express();
 
 const PORT = 3000;
 
 app.use(express.json());
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Welcome to ChatBot');
